77cf571b6aa9c750729c14c750e9b29f1487a08e
Vlad Khalphin <creepie1357@gmail.com>
2026-03-18 17:12:28 +0200

    "%MSG%"

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d F:\Amber_solutions_Kira\Trinity
+git rm --cached __*.ps1 __*.txt deploy.bat 2>nul
 git add .
-git commit -m "fix: receipt settings tranzila fields"
+git commit -m "chore: remove temp scripts from repo"
 git push
 echo DONE

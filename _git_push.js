62a624b8d19888de81955f865a8593999b6493d3
Vlad Khalphin <creepie1357@gmail.com>
2026-03-27 16:47:18 +0300

    chore: ignore temp deploy script

--- _git_push.js
+++
@@ -1,5 +0,0 @@
-const {execSync} = require('child_process');
-process.chdir('F:\\Amber_solutions_Kira\\Trinity');
-execSync('git add -A', {stdio: 'inherit'});
-execSync('git commit -m "feat: demo limits 403 standard format, global interceptor, SPA keepPreviousData"', {stdio: 'inherit'});
-execSync('git push origin main', {stdio: 'inherit'});

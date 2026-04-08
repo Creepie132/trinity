683831381a4c3a44b5cf0f592c1bbe0830393365
Vlad Khalphin <creepie1357@gmail.com>
2026-04-08 13:50:19 +0300

    chore: remove temp commit.bat

--- cleanup.bat
+++ cleanup.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d F:\Amber_solutions_Kira\Trinity
+del commit.bat
 git add -A
-git commit -m "feat: VisitActionButtons global standard for visit cards"
+git commit -m "chore: remove temp commit.bat"
 git push origin main
